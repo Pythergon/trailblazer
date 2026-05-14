0e72303af16db61319cc1a016247f30ad632a6ce
Pythergon <pythonissupreme@gmail.com>
2026-05-14 16:19:08 -0500

    modified:   server.js 	modified:   test.pdf

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const PDFDocument = require('pdfkit');
 const bwipjs = require('bwip-js');
 const fs = require('fs');
 
-// doc.fontSize(25).text(`Order for: ${jsonData.name}`, 100, 100);
-
 const port = 3000
 const ip_addr = '10.0.0.200'
 
@@ -18,34 +16,43 @@ app.post('/', async (req, res) => {
     console.log(`Data: ${JSON.stringify(req.body)}`);
     let data = JSON.parse(JSON.stringify(req.body));
 
-    const doc = new PDFDocument({size: [72, 288], margin: 0, layout: 'landscape'});
-    // console.log(`Data Code: ${data.Code}`);
+    const doc = new PDFDocument({size: [72, 288], margin: 3, layout: 'landscape'});
 
     doc.pipe(fs.createWriteStream(`${'test'}.pdf`));
     doc.fontSize(data.fontSize || 14);
 
-    try {
-        // Generate the barcode buffer
-        const png = await bwipjs.toBuffer({
-            bcid: 'code128',
-            text: data.barcode.replaceAll('#', '').padStart(8, "0") || '00000000',
-            scale: 3,
-            height: 40,
-        });
+    let png;
 
-        // Now png is in scope and the data is ready
-        doc.image(png, {align: 'center', width: 72});
-        
+    try {
+        png = await bwipjs.toBuffer({
+            bcid: 'code128',
+            text: data.barcode || '00000000',
+            scale: 5,         
+            height: 15,     
+            sizunit: 'mm' 
+        });
+        console.log("Barcode generated successfully");
     } catch (err) {
         console.error("Barcode Generation Error:", err);
     }
-
-    // doc.fontSize(7).text(JSON.stringify(data), {align: 'center'});
+    
+    let data_str = '';
     for (const [key, value] of Object.entries(data)) {
-        if (!(key === 'fontSize')) {
-        doc.text(`${key}: ${value}`, {align: 'center'});
+        if (!(key === 'fontSize' || key === 'barcode')) {
+        // doc.text(`${key}: ${value}`, {align: 'center'});
+        data_str += `${key}: ${value}\n`;
         }
     }
+
+    doc.image(png, { 
+        align: 'left',
+        width: 72,  
+        height: 66 
+    });
+
+    doc.text(data_str, 78, 6, {
+        align: 'left',
+    });
 
     doc.end();
 
